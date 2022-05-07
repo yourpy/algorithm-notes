b76d75de4fc53dcbe8a3642ce079fe9ecc5c20fd
yourpy <41859161+yourpy@users.noreply.github.com>
2022-05-07 17:09:45 +0800

    Create InsertionSort.java

---
+++ sort/InsertionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Code06_InsertionSort {
+    public static void insertionSort(int[] arr) {
+        if (arr == null || arr.length() < 2) {
+            return;
+        }
+        
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    
+    // i和j,数交换
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+}
