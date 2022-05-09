472cd3cddd67fc241dd2ad39f76a4e460575ce62
yourpy <41859161+yourpy@users.noreply.github.com>
2022-05-09 18:03:49 +0800

    Update SelectionSort.java

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 public class SelectionSort {
     
     public static void selectionSort(int[] arr) {
-        if (arr == null || arr.length() < 2) {
+        if (arr == null || arr.length < 2) {
             return;
         }
         
-        for (int i = 0; i < arr.length() - 1; i++){
+        for (int i = 0; i < arr.length - 1; i++){
             int minIndex = 0;
             for (int j = i + 1; j < arr.length(); j++) {
                 if (arr[j] < arr[minIndex]) {
