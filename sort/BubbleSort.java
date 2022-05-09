59333b8768cd1c2750aa9b3b511e9320ea731868
yourpy <41859161+yourpy@users.noreply.github.com>
2022-05-09 18:03:09 +0800

    Update BubbleSort.java

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 			return;
 		}
         
-        for (int end = arr.length() - 1; end >=0; end--) {
+        for (int end = arr.length - 1; end >=0; end--) {
             for (int i = 0; i < end; i++) {
                 if (arr[i] > arr[i + 1]) {
                     swap(arr, i, i + 1);
