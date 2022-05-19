cdae2b2572e248329e8eef32453ef51eaf9d22cf
yourpy <41859161+yourpy@users.noreply.github.com>
2022-05-19 17:20:11 +0800

    Update SelectionSort.java

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -8,7 +8,7 @@ public class SelectionSort {
         }
         
         for (int i = 0; i < arr.length - 1; i++){
-            int minIndex = 0;
+            int minIndex = i;
             for (int j = i + 1; j < arr.length(); j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
